e6630cb5936afca6bb74a2cf0120e32e00ba98b2
Justin Mao-Jones <justinmaojones@gmail.com>
2015-10-15 11:05:40 -0400

    added logp(x) calc, changed what gets saved, model params now adjustable, commented out learning rate decay, added adam, more dropout, added Maxout

--- train.lua
+++ train.lua
@@ -6,33 +6,55 @@ require "cunn"
 require "cutorch"
 require "optim"
 require "GaussianReparam"
+require 'Maxout'
+require 'hdf5'
 disp = require "display"
 
+local devicenum = devicenum or 1
+cutorch.setDevice(devicenum)
 torch.manualSeed(1)
 cutorch.manualSeed(1)
+---------------- Train Params ------------
+local savedir = savedir or 'save'
+local optimMethod = optimMethod or 'adam'
+local savebestmodel = savebestmodel or true
+local datadir = datadir or './'
+print('save directory = ' .. savedir)
+print('data directory = ' .. datadir)
+
 ---------------- Load Data ---------------
-data=loadBinarizedMNIST(true)
+data=loadBinarizedMNIST(true,datadir)
 
 ---------------- Model Params. -----------
-local dim_input = 784
-local dim_hidden= 400
-local dim_stochastic = 100
-local nonlinearity   = nn.ReLU
+local dim_input      = 784
+local dim_hidden     = dim_hidden or 400
+local dim_stochastic = dim_stochastic or 40
+local dropout        = dropout or false
+local maxoutWindow   = maxoutWindow or 4
+local nonlinearity   = nonlinearity or nn.ReLU
+local S              = S or 200
 
 --------- Recognition. Network -----------
 local var_inp = nn.Identity()()
 local dropped_inp = nn.Dropout(0.25)(var_inp)
-local q_1 = nonlinearity()(nn.Linear(dim_input,dim_hidden)(dropped_inp))
-local q_hid_1 = nonlinearity()(nn.Linear(dim_hidden,dim_hidden)(q_1))
-local q_hid_2 = nonlinearity()(nn.Linear(dim_hidden,dim_hidden)(q_hid_1))
-local mu  = nn.Linear(dim_hidden,dim_stochastic)(q_hid_2)
-local logsigma  = nn.Linear(dim_hidden,dim_stochastic)(q_hid_2)
-local reparam   = nn.GaussianReparam(dim_stochastic)
-print (reparam.KL)
-local z  = reparam({mu,logsigma})
+q_1 = nn.Maxout(dim_input,dim_hidden,maxoutWindow)(dropped_inp)
+if dropout then
+    q_hid_1 = nn.Maxout(dim_hidden,dim_hidden,maxoutWindow)(nn.Dropout(dropout)(q_1))
+    q_hid_2 = nn.Maxout(dim_hidden,dim_hidden,maxoutWindow)(nn.Dropout(dropout)(q_hid_1))
+else
+    q_hid_1 = nn.Maxout(dim_hidden,dim_hidden,maxoutWindow)(q_1)
+    q_hid_2 = nn.Maxout(dim_hidden,dim_hidden,maxoutWindow)(q_hid_1)
+end
+q_hid_1 = nn.Maxout(dim_hidden,dim_hidden,maxoutWindow)(q_1)
+q_hid_2 = nn.Maxout(dim_hidden,dim_hidden,maxoutWindow)(q_hid_1)
+mu  = nn.Linear(dim_hidden,dim_stochastic)(q_hid_2)
+logsigma  = nn.Linear(dim_hidden,dim_stochastic)(q_hid_2)
+reparam   = nn.GaussianReparam(dim_stochastic)
+z = reparam({mu,logsigma})
 local var_model = nn.gModule({var_inp},{z})
 
 --------- Generative Network -------------
+torch.manualSeed(1)
 local gen_inp = nn.Identity()()
 local hid1 = nonlinearity()(nn.Linear(dim_stochastic,dim_hidden)(gen_inp))
 local hid2 = nonlinearity()(nn.Linear(dim_hidden,dim_hidden)(hid1))
@@ -49,15 +71,16 @@ crit.sizeAverage = false
 
 --------- Setup for Training/Viz.---------
 img_format,format = setupDisplay()
-setupFolder('./save')
+setupFolder(savedir)
 mlp:cuda()
 crit:cuda()
 data.train_x = data.train_x:cuda()
 data.test_x  = data.test_x:cuda()
 parameters, gradients = mlp:getParameters()
-config = {
+config = optimconfig or {
     learningRate = 0.00005,
 }
+print(config)
 batchSize = 100
 state = {}
 
@@ -81,6 +104,39 @@ function eval(dataset)
 	mlp:training()
 	return (nll+reparam.KL)/dataset:size(1),probs
 end
+--------- Test p(v) via importance sampling ---------
+-- see appendix E in Rezende, D. J., Mohamed, S., and Wierstra, D. Stochastic backpropagation and approximate inference in deep generative models. In ICML, 2014.
+function estimate_testNLL(dataset,S)
+        mlp:evaluate()
+        local S = S or 200
+        local logp = nil
+        for s = 1,S do
+                local probs = mlp:forward(dataset)
+				-- ll = p(v|z_s) ~ calc assumes each pixel output is conditional independent given latent states
+                local ll = (torch.cmul(torch.log(probs+1e-12),dataset)+torch.cmul(torch.log(probs*(-1)+1+1e-12),dataset*(-1)+1)):sum(2)
+				-- latent = z_s = sampled z|v
+                local latent = z.data.module.output
+                -- logprior is standard normal
+                local logprior = (torch.pow(latent,2):sum(2)+dim_stochastic*math.log(2*math.pi))*(-0.5)
+				-- assume logsigma is log(sigma^2)
+                local logsig = logsigma.data.module.output
+                local m = mu.data.module.output
+                local logq = ((torch.exp(logsig*(-1)):cmul(torch.pow(latent-m,2))):sum(2)+logsig:sum(2)*(0.5)+dim_stochastic*math.log(2*math.pi))*(-0.5)
+				-- log p_s = log(p(v|z_s)*p(z_s)/q(z_s|v)) 
+                local logp_s = ll+logprior-logq
+				-- logp = [logp_1,...,logp_S]
+                if logp ~= nil then
+                        logp = nn.JoinTable(2):cuda():forward{logp_s,logp}
+                else
+                        logp = logp_s
+                end
+        end
+        local a = logp:max(2)
+		-- computing logsumexp: https://hips.seas.harvard.edu/blog/2013/01/09/computing-log-sum-exp/
+        local logsumexp = a + torch.log(torch.exp(logp-nn.Replicate(S,2):cuda():forward(a)):sum(2)) - math.log(S)
+        local logpv = logsumexp:mean()
+        return logpv
+end
 --------- Stitch Images Together ---------
 function stitch(probs,batch)
 	local imgs = {}
@@ -92,12 +148,13 @@ end
 
 -------------- Training Loop -------------
 for epoch =1,5000 do 
+	collectgarbage()
     local upperbound = 0
 	local trainnll = 0
     local time = sys.clock()
     local shuffle = torch.randperm(data.train_x:size(1))
-	if epoch==100 then config.learningRate = 5e-5 end
-	if epoch > 30 then config.learningRate = math.max(config.learningRate / 1.000005, 0.000001) end
+	--if epoch==100 then config.learningRate = 5e-5 end
+	--if epoch > 30 then config.learningRate = math.max(config.learningRate / 1.000005, 0.000001) end
     --Make sure batches are always batchSize
     local N = data.train_x:size(1) - (data.train_x:size(1) % batchSize)
     local N_test = data.test_x:size(1) - (data.test_x:size(1) % batchSize)
@@ -127,7 +184,13 @@ for epoch =1,5000 do
             return upperbound, gradients+(parameters*0.05)
         end
 
-        parameters, batchupperbound = optim.rmsprop(opfunc, parameters, config, state)
+	if optimMethod == 'rmsprop' then
+		parameters, batchupperbound = optim.rmsprop(opfunc, parameters, config, state)
+	elseif optimMethod == 'adam' then
+		parameters, batchupperbound = optim.adam(opfunc, parameters, config, state)
+	else
+		error('unknown optimization method')
+	end
         upperbound = upperbound + batchupperbound[1]
     end
 	
@@ -140,6 +203,25 @@ for epoch =1,5000 do
 
     if epoch % 10  == 0 then
     	print("\nEpoch: " .. epoch .. " Upperbound: " .. upperbound/N .. " Time: " .. sys.clock() - time)
+        testlogpv = estimate_testNLL(data.test_x,S)*(-1)
+        print("logp = " .. testlogpv)
+		if savebestmodel then
+			if bestmodel == nil then
+				bestmodel = testlogpv
+			elseif testlogpv < bestmodel then
+				local model_save = {
+					mlp = mlp,
+					flow = flow,
+					mu = mu,
+					logsigma = logsigma,
+					z = z,
+					var_model = var_model,
+					gen_model = gen_model
+				}
+				torch.save(paths.concat(savedir,'model.t7'),model_save)
+				bestmodel = testlogpv
+			end
+		end
 		--Display reconstructions and samples
 		img_format.title="Train Reconstructions"
 		img_format.win = id_reconstr
@@ -163,11 +245,25 @@ for epoch =1,5000 do
 		img_format.win = id_mp
 		id_mp =  disp.images(mp,img_format)
 		print ("Train NLL:",trainnll/N,"Test NLL: ",testnll)
-        torch.save('save/parameters.t7', parameters)
-        torch.save('save/state.t7', state)
-        torch.save('save/upperbound.t7', torch.Tensor(upperboundlist))
+		print ("saving to directory: " .. savedir)
+		if testupperboundlist then
+			testupperboundlist = torch.cat(testupperboundlist,torch.Tensor(1,1):fill(testnll),1)
+			testlogpvlist = torch.cat(testlogpvlist,torch.Tensor(1,1):fill(testlogpv),1)
+		else
+			testupperboundlist = torch.Tensor(1,1):fill(testnll)
+			testlogpvlist = torch.Tensor(1,1):fill(testlogpv)
+		end
+		torch.save(paths.concat(savedir,'parameters.t7'), parameters)
+		torch.save(paths.concat(savedir,'state.t7'), state)
+		torch.save(paths.concat(savedir,'upperbound.t7'), torch.Tensor(upperboundlist))
+		writeFile = hdf5.open(paths.concat(savedir,'upperbounds.h5'),'w')
+		writeFile:write('train',upperboundlist)
+		writeFile:write('test',testupperboundlist)
+		writeFile:write('test_logpv',testlogpvlist)
+		writeFile:close()
+		torch.save(paths.concat(savedir,'upperbound_test.t7'),testupperboundlist)
 		local s,mp = getsamples()
-		torch.save('save/samples.t7',s)
-		torch.save('save/mean_probs.t7',mp)
+		torch.save(paths.concat(savedir,'samples.t7'),s)
+		torch.save(paths.concat(savedir,'mean_probs.t7'),mp)
     end
 end
